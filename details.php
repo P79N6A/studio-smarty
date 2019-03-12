f1872ba507c597db866e4edac09f614b496fa4f0
pengjian.wang@xstudy.com.cn <408648033@qq.com>
2019-03-12 13:54:58 +0800

    class

--- details.php
+++ details.php
@@ -6,11 +6,25 @@
  */
 require './libs/Smarty.class.php';
 require './configs/configs.php';
+require './ApiUtil.php';
+
 $smarty = new Smarty;
 $smarty->cache_dir = 'cache';
 $smarty->debugging = false;
 $smarty->caching = false;
 $smarty->cache_lifetime = 120;
+
+
+$api = new ApiUtil();
+$data = $api::appGet($configs['uri'].'/index/getinfo/zzw_get_learninfo/id/'.$_GET['id']);
+$smarty->assign('data',$data['data'][0]);
+
+$text = $data['data'][0]['detail_text'];
+$reg = '/<img src="/';
+$str = '<img src="'.$configs['uri'];
+$smarty->assign('text',preg_replace($reg,$str,$text));
+
+
 $smarty->assign('nav',$configs['nav']);
 $smarty->assign('uri',$configs['uri']);
 $smarty->display('details.tpl');
