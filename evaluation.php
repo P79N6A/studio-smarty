e4574e8fcbd13845efc58060f2c3b10096866447
pengjian.wang@xstudy.com.cn <408648033@qq.com>
2019-03-13 11:37:31 +0800

    export

--- evaluation.php
+++ evaluation.php
@@ -5,37 +5,40 @@
  * @package Example-application
  */
 require './configs/configs.php';
-if(isset($_POST['submit']) && $_POST['submit'] == 'yes'){
-
-    $postData = $_POST;
-    $tmp = [];
-    if($postData && is_array($postData)){
-        foreach ($postData as $key=>$value){
-            $tmp[$key] = safe_replace($value);
-        }
-        $tmp['id'] = '';
-    }else{
-        die(json_encode(['status'=>500,'messages'=>'信息错误']));
-    }
+require './libs/Smarty.class.php';
+if($_POST){
 
 
     $dsn = "mysql:dbname={$configs['databases']['dbname']};host={$configs['databases']['host']}";
     $dbh = new PDO($dsn, $configs['databases']['username'], $configs['databases']['password']);
     $dbh->exec('set names utf8');
 
+    if(isset($_POST['submit']) && $_POST['submit'] == 'yes'){
+
+        $postData = $_POST;
+        $tmp = [];
+        if($postData && is_array($postData)){
+            foreach ($postData as $key=>$value){
+                $tmp[$key] = safe_replace($value);
+            }
+            $tmp['id'] = '';
+        }else{
+            die(json_encode(['status'=>500,'messages'=>'信息错误']));
+        }
 //    $sql = 'select * from evaluation';
 //    $stmt = $dbh->prepare($sql)->execute();
 //    $res = $stmt->fetch();
 //    var_dump($res);exit;
 
 
-    $sql = "INSERT INTO `evaluation` (`student_name` ,`student_school`,`student_major`,`student_gpa`,`student_langscore`,`student_weixin`,`student_tel`,`student_email`,`student_file`,`student_country`,`student_applyschool`,`student_applymajor`,`student_applytype`,`student_time`) VALUES ('{$tmp['student_name']}', '{$tmp['student_school']}','{$tmp['student_major']}','{$tmp['student_gpa']}','{$tmp['student_langscore']}','{$tmp['student_weixin']}','{$tmp['student_tel']}','{$tmp['student_email']}','{$tmp['student_file']}','{$tmp['student_country']}','{$tmp['student_applyschool']}','{$tmp['student_applymajor']}','{$tmp['student_applytype']}','{$tmp['student_time']}')";
-    $stmt = $dbh->exec($sql);
-    if(empty($stmt) || $dbh->errorCode()!= 0000){
-        die(json_encode(['status'=>500,'messages'=>'信息错误']));
-    }else{
-        die(json_encode(['status'=>200,'messages'=>'提交成功']));
-    }
+        $sql = "INSERT INTO `evaluation` (`student_name` ,`student_school`,`student_major`,`student_gpa`,`student_langscore`,`student_weixin`,`student_tel`,`student_email`,`student_file`,`student_software`,`student_country`,`student_applyschool`,`student_applymajor`,`student_applytype`,`student_time`) VALUES ('{$tmp['student_name']}', '{$tmp['student_school']}','{$tmp['student_major']}','{$tmp['student_gpa']}','{$tmp['student_langscore']}','{$tmp['student_weixin']}','{$tmp['student_tel']}','{$tmp['student_email']}','{$tmp['student_file']}','{$tmp['student_software']}','{$tmp['student_country']}','{$tmp['student_applyschool']}','{$tmp['student_applymajor']}','{$tmp['student_applytype']}','{$tmp['student_time']}')";
+        $stmt = $dbh->exec($sql);
+        var_dump($dbh->errorInfo());
+        if(empty($stmt) || $dbh->errorCode()!= 0000){
+            die(json_encode(['status'=>500,'messages'=>'信息错误']));
+        }else{
+            die(json_encode(['status'=>200,'messages'=>'提交成功']));
+        }
 //    echo '<pre>';
 //    var_dump($stmt);
 //    var_dump(empty($stmt));
@@ -46,22 +49,32 @@ if(isset($_POST['submit']) && $_POST['submit'] == 'yes'){
 //    exit;
 //
 //    die(json_encode($result));
-}elseif (isset($_POST['uploads']) && $_POST['uploads'] == 'yes'){
-    require_once ('./vendor/uploads/fileUploadPlugin.php');
-    $uploadsObj = new fileUploadPlugin();
-    $res = $uploadsObj->upload('file',$_FILES);
-    die($res);
+    }elseif (isset($_POST['uploads']) && $_POST['uploads'] == 'yes'){
+        require_once ('./vendor/uploads/fileUploadPlugin.php');
+        $uploadsObj = new fileUploadPlugin();
+        $res = $uploadsObj->upload('file',$_FILES);
+        die($res);
+    }
 }else{
 
-    require './libs/Smarty.class.php';
     $smarty = new Smarty;
     $smarty->cache_dir = 'cache';
     $smarty->debugging = false;
     $smarty->caching = false;
     $smarty->cache_lifetime = 120;
-
     $smarty->assign('nav',$configs['nav']);
-    $smarty->display('evaluation.tpl');
+
+    if(isset($_GET['export']) && $_GET['export'] === 'Yes'){
+
+        $data = [];
+        $smarty->assign('data',$data);
+        $smarty->display('export.tpl');
+
+    }else{
+        $smarty->assign('nav',$configs['nav']);
+        $smarty->display('evaluation.tpl');
+    }
+
 }
 
 
---
+++ templates/export.tpl
@@ -0,0 +1,60 @@
+{include file="header.tpl"}
+<link rel="stylesheet" href="public/css/bootstrap.min.css">
+<style>
+    .table th, .table td {
+        text-align: center;
+    }
+</style>
+<div class="h15"></div>
+<div class="body">
+
+    <table id="datatables" class="table table-striped table-bordered table-hover" cellspacing="0" width="100%">
+        <thead>
+            <tr>
+                <th>姓名</th>
+                <th>学校</th>
+                <th>专业</th>
+                <th>平均成绩</th>
+                <th>语言成绩</th>
+                <th>微信号</th>
+                <th>手机号</th>
+                <th>邮箱</th>
+                <th>作品</th>
+                <th>软件</th>
+                <th>申请国家</th>
+                <th>目标学校</th>
+                <th>申请专业</th>
+                <th>申请类别</th>
+                <th>入学时间</th>
+            </tr>
+        </thead>
+
+        <tbody>
+        {if $data}
+            {foreach $data as $evaluation}
+            <tr>
+                <td>{$evaluation['student_name']}</td>
+                <td>{$evaluation['student_school']}</td>
+                <td>{$evaluation['student_major']}</td>
+                <td>{$evaluation['student_gpa']}</td>
+                <td>{$evaluation['student_langscore']}</td>
+                <td>{$evaluation['student_weixin']}</td>
+                <td>{$evaluation['student_tel']}</td>
+                <td>{$evaluation['student_email']}</td>
+                <td>{$evaluation['student_file']}</td>
+                <td>{$evaluation['student_software']}</td>
+                <td>{$evaluation['student_country']}</td>
+                <td>{$evaluation['student_applyschool']}</td>
+                <td>{$evaluation['student_applymajor']}</td>
+                <td>{$evaluation['student_applytype']}</td>
+                <td>{$evaluation['student_time']}</td>
+            </tr>
+            {/foreach}
+        {else}
+            <tr><td colspan="15">暂无申请信息</td></tr>
+        {/if}
+        </tbody>
+    </table>
+</div>
+
+{include file="foot.tpl"}
